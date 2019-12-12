0596c6651fc4a04449581ad4097e3fabfa1db908
neptech4u <bhagyasah4u@gmail.com>
2019-12-12 16:51:14 +0545

    server endpoint changed

--- packages/web/src/config/index.js
+++ packages/web/src/config/index.js
@@ -1,6 +1,6 @@
 export const APP_PRIMARY_COLOR = '#00B0FF';
 export const APP_TEXT_COLOR = 'white';
-// export const ENDPOINT = 'http://159.89.150.216:4002/';
-export const ENDPOINT = 'http://localhost:4003';
+export const ENDPOINT = 'http://159.89.150.216:4003';
+// export const ENDPOINT = 'http://localhost:4003';
 export const WINDOWS_HEIGHT = window.innerHeight;
 export const WINDOWS_WIDTH = window.innerWidth;
