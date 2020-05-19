a71b148e1c50d0b03a605c5a8ffc1c32484a25f8
neptech4u <bhagyasah4u@gmail.com>
2020-05-19 09:00:51 +0545

    test

--- packages/web/src/screens/contactus/index.js
+++ packages/web/src/screens/contactus/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { Tabs, Tab } from '@blueprintjs/core';
 import NavBar from '../home/components/navbar';
+
 
 class ContactUs extends React.Component {
   constructor(props) {
     super(props);
-    this.state={}
+    this.state = { tabId: 'ng'};
+  }
+
+  handleTabChange = (tabId) => {
+    this.setState({ tabId: tabId });
   }
 
   render() {
     return (
       <div>
-        <NavBar />
+        <Tabs id="TabsExample" onChange={this.handleTabChange} selectedTabId={this.state.tabId}>
+          <Tab id="ng" title="Angular" panel={<div>Angular</div>} />
+          <Tab id="mb" title="Ember" panel={<div>Ember</div>} />
+          <Tab id="rx" title="React" panel={<div>React</div>} />
+          <Tab id="bb" title="Backbone" panel={<div>Backbone</div>} />
+        </Tabs>
       </div>
     );
   }
