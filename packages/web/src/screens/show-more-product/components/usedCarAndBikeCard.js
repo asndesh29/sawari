29bc50c8eaf080ea67fa6f02f551d3911f4a0b84
Bhagya sah <bhagyasah4u@gmail.com>
2020-07-06 10:44:26 +0545

    used car functinality upgraded

--- packages/web/src/screens/show-more-product/components/usedCarAndBikeCard.js
+++ packages/web/src/screens/show-more-product/components/usedCarAndBikeCard.js
@@ -6,9 +6,8 @@ import { ENDPOINT } from '../../../config';
 export default (obj) => {
   return (
     <Card
-      className="product-card"
+      className="used-card"
       // onClick={() => cardOnClickHandler(obj, 'used')}
-      style={{ maxHeight: 400, objectFit: 'fill' }}
     >
       <div className="image-container" style={{ cursor: 'pointer' }}>
         <Link to={`/used-vehicle/details/${obj.model.replace(/\s/g, '')}-${obj.id}`.toLocaleLowerCase()}>
@@ -17,7 +16,14 @@ export default (obj) => {
       </div>
       <div className="description">
         <span style={{ fontWeight: 100, fontSize: 20 }}>{obj.model}</span>
-        <span style={{ fontWeight: 100, color: '#ff4202', fontStyle: 'italic' }}>{`NRs. ${obj.expectedPrice} /-`}</span>
+        <div style={{ color: '#828282', marginLeft: 10 }}>
+          <span style={{ marginLeft: 10 }}>{obj.city}</span>
+          <span style={{ marginLeft: 10 }}>|</span>
+          <span style={{ marginLeft: 10 }}>{`${obj.makeYear}`}</span>
+          <span style={{ marginLeft: 10 }}>|</span>
+          <span style={{ marginLeft: 10 }}>{`${obj.kmsDriven}Kms`}</span>
+        </div>
+        <span style={{ fontWeight: 100, color: '#ff4202', fontStyle: 'italic' }}>{`रु. ${obj.expectedPrice} लाख`}</span>
       </div>
     </Card>
   );
